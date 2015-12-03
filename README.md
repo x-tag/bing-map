aa9ee46289e5262f2461162592c203552bde4fb1
Daniel Buchner <danieljb2@gmail.com>
2015-12-03 11:32:00 -0800

    first commit

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-# About
-
-This repo is an X-Tag custom web-component stub that can be used as a starting point when building new elements.  It provides that basic tools and grunt tasks to get started.
-
-# Yo Generator
-
-If you're creating a new component, checkout our Yeoman Generator.
-
-```
-https://github.com/x-tag/yo-x-tag-generator
-```
 
 # Dev Setup
 
@@ -20,13 +9,3 @@ $ bower install	// install components
 $ grunt build   // build the dependencies
 
 ```
-
-# Links
-
-[X-Tags Docs](http://x-tags.org/docs)
-
-[Guide for creating X-Tag Components](https://github.com/x-tag/core/wiki/Creating-X-Tag-Components)
-
-[Using X-Tag components in your applications](https://github.com/x-tag/core/wiki/Using-our-Web-Components-in-Your-Application)
-
-
--- src/main.js
+++ src/main.js
@@ -1,23 +1,48 @@
-(function(){  
+(function(){
 
-  // Create your component here
-  // http://x-tags.org/docs
-  
-  xtag.register('x-stub', {
+  // THIS IS TERRIBLE, FILE A TICKET WITH THE BING MAPS TEAM *ASAP*
+
+  document.open();
+  document.write('<script src="http://ecn.dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=7.0"></script>');
+  document.close();
+
+  function setView(force){
+    if (this.xtagComponentReady || force == 'force') {
+      if (!this.xtag.instance) this.xtag.instance = new Microsoft.Maps.Map(this, {
+        credentials: 'An_Kp3MBJt-AX1zwKbU3X15dVAdDbE9k6PXp2DSmSEH6chc6zN1HEkiMXDPKOja8',
+        mapTypeId: Microsoft.Maps.MapTypeId.road
+      });
+      var center = this.xtag.center || [];
+      this.xtag.instance.setView({
+        mapTypeId: Microsoft.Maps.MapTypeId[this.type || 'road'],
+        center: new Microsoft.Maps.Location(center[0] || 55, center[1] || -1.5),
+        zoom: Number(this.zoom) || 7
+      });
+    }
+  }
+
+  xtag.register('x-bing-map', {
     lifecycle: {
-      created: function() {},
-      inserted: function() {},
-      removed: function() {},
-      attributeChanged: function() {}
-    }, 
-    events: { 
-    
+      created: function() {
+        setView.call(this, 'force');
+      }
     },
     accessors: {
-      
-    }, 
-    methods: {
-      
+      type: {
+        attribute: {},
+        set: setView
+      },
+      zoom: {
+        attribute: {},
+        set: setView
+      },
+      center: {
+        attribute: {},
+        set: function(val){
+          this.xtag.center = JSON.parse('['+ val +']');
+          setView.call(this);
+        }
+      }
     }
   });
 
